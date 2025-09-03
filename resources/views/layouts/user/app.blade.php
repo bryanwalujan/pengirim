c782791cf2319dba2cd3a3e02181b3ae1d499734
patrickrompas20 <louisrompas20@gmail.com>
2025-09-03 18:29:52 +0800

    refactor: remove unused Swiper and PureCounter scripts for cleaner code

--- resources/views/layouts/user/app.blade.php
+++ resources/views/layouts/user/app.blade.php
@@ -25,7 +25,6 @@
     <link href="{{ asset('user/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('user/assets/vendor/aos/aos.css') }}" rel="stylesheet">
     <link href="{{ asset('user/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('user/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
 
     {{-- SweetAlert2 --}}
@@ -99,8 +98,6 @@
     <script src="{{ asset('user/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('user/assets/vendor/aos/aos.js') }}"></script>
     <script src="{{ asset('user/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-    <script src="{{ asset('user/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-    <script src="{{ asset('user/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
 
     {{-- Pdf js --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js"></script>
